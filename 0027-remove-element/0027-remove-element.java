ab98de0c9bc7129cabf64b27fe7f2ef0f4bb93a4
Younggil An <higiri@naver.com>
2023-12-01 00:08:06 +0900

    Time: 0 ms (100.00%), Space: 41 MB (40.19%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,6 +1,5 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-
         int p1 = 0;
         int p2 = nums.length - 1;
         int tmp = 0;
@@ -14,9 +13,10 @@ class Solution {
                 }
                 
                 if (p1 != p2) {
-                    tmp = nums[p1];
+                    // tmp = nums[p1];
                     nums[p1] = nums[p2];
-                    nums[p2] = tmp;
+                    p2--;
+                    // nums[p2] = tmp;
                 } else {
                     return p1;
                 }
