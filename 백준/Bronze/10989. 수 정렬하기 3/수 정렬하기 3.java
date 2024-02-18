9b235a1418dc227923113346bcb1035fb6e57ae4
Younggil An <higiri@naver.com>
2024-02-18 18:20:09 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 508 ms, Memory: 222520 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,30 +1,49 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
-class Main {
-
-    public static final int NUMS_RANGE = 10000;
-
+public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[NUMS_RANGE];
-        for (int i = 0; i < n; i++) {
-            nums[Integer.parseInt(br.readLine()) - 1]++;
+        int N = 0;
+        int in = System.in.read();
+        while (in > 32) {
+            N = (N << 3) + (N << 1) + (in & 15);
+            in = System.in.read();
         }
-        br.close();
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i = 0; i < NUMS_RANGE; i++) {
-            for (int j = 0; j < nums[i]; j++) {
-                bw.write(i + 1 + "");
-                bw.newLine();
+        byte[] buf = new byte[N * 9];
+        int idx = -1;
+        System.in.read(buf, 0, buf.length);
+        int[] table = new int[10001];
+        while (--N != -1) {
+            int num = 0;
+            while (buf[++idx] > 32)
+                num = (num << 3) + (num << 1) + (buf[idx] & 15);
+            ++table[num];
+        }
+        idx = 0;
+        for (int i = 0; i <= 10000; i++)
+            if (table[i] > 0) {
+                int len;
+                if (i < 10)
+                    len = 1;
+                else if (i < 100)
+                    len = 2;
+                else if (i < 1000)
+                    len = 3;
+                else if (i < 10000)
+                    len = 4;
+                else
+                    len = 5;
+                int num = i;
+                for (int loc = idx + len - 1; loc != idx - 1; loc--) {
+                    buf[loc] = (byte)((num % 10) + '0');
+                    num /= 10;
+                }
+                buf[idx + len] = '\n';
+                for (int j = 0; j <= len; j++)
+                    for (int k = 1; k < table[i]; k++)
+                        buf[idx + k * (len + 1) + j] = buf[idx + j];
+                idx += table[i] * (len + 1);
             }
-        }
-        bw.flush();
-        bw.close();
+            
+        System.out.write(buf, 0, idx);
     }
 }
