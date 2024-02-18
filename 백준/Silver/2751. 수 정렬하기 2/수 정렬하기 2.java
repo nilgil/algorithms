b8765577bc5e9883bfc90c2e5537fb6174eda5af
Younggil An <higiri@naver.com>
2024-02-18 20:35:10 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 780 ms, Memory: 93772 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,28 +1,30 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
-class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        boolean[] nums = new boolean[2_000_001]; // -1_000_000 ~ 1_000_000 -> 0의 위치 = [1_000_000]
-        for (int i = 0; i < n; i++) {
-            nums[Integer.parseInt(br.readLine()) + 1_000_000] = true;
-        }
-        br.close();
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i]) {
-                bw.write((i - 1_000_000) + "\n");
-            }
-        }
-        bw.flush();
-        bw.close();
-    }
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        
+		/*
+		  -1000000 ~ 1000000
+		  기준점 0 = index 100000 으로 생각
+		*/
+		boolean[] arr = new boolean[2000001];	
+        
+		int N = Integer.parseInt(br.readLine());
+        
+		for(int i = 0; i < N; i++) {
+			arr[Integer.parseInt(br.readLine()) + 1000000] = true;
+		}
+ 
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i]) {
+				sb.append((i - 1000000)).append('\n');
+			}
+		}
+		System.out.print(sb);
+	}
 }
