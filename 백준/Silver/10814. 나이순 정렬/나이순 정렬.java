6ba66868fac2c35b84c4cdc19e60e8c9e48b8c8f
Younggil An <higiri@naver.com>
2024-02-19 20:34:26 +0900

    [Silver V] Title: 나이순 정렬, Time: 452 ms, Memory: 41872 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 class Main {
@@ -10,28 +8,28 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] users = new String[201];
+        StringBuilder[] builders = new StringBuilder[201];
 
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int age = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
-            users[age] = users[age] == null ? name : users[age] + " " + name;
+
+            if (builders[age] == null) {
+                builders[age] = new StringBuilder();
+            }
+
+            builders[age].append(age).append(' ').append(name).append('\n');
         }
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i = 1; i < users.length; i++) {
-            String user = users[i];
-            if (user == null) {
-                continue;
-            }
-            StringTokenizer st = new StringTokenizer(user);
-            while (st.hasMoreTokens()) {
-                bw.write(i + " " + st.nextToken());
-                bw.newLine();
+        StringBuilder sb = new StringBuilder();
+        for (StringBuilder builder : builders) {
+            if (builder != null) {
+                sb.append(builder);
             }
         }
-        bw.flush();
+
+        System.out.print(sb);
     }
 }
