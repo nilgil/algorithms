75d7224a6992326d56de1b6786e4d43de6f1af3a
Younggil An <higiri@naver.com>
2024-02-21 19:26:25 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 652 ms, Memory: 49840 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main {
@@ -12,25 +9,25 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-
-        Map<Integer, List<Integer>> map = new LinkedHashMap<>();
-
+        int[][] array = new int[n][2];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            List<Integer> nums = map.getOrDefault(x, new ArrayList<>());
-            nums.add(y);
-            map.put(x, nums);
+            array[i][0] = Integer.parseInt(st.nextToken());
+            array[i][1] = Integer.parseInt(st.nextToken());
         }
-        int[] array = map.keySet().stream().mapToInt(value -> value).sorted().toArray();
 
-        for (int x : array) {
-            List<Integer> nums = map.get(x);
-            nums.stream().sorted().forEach(y -> sb.append(x).append(' ').append(y).append('\n'));
+        Arrays.sort(array, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            } else {
+                return o1[0] - o2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(array[i][0]).append(' ').append(array[i][1]).append('\n');
         }
         System.out.println(sb);
     }
