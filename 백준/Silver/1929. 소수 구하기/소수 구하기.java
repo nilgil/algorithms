9ba236048dc39950cd023e318d90e93ca275f8d2
Younggil An <higiri@naver.com>
2024-03-11 03:53:26 +0900

    [Silver III] Title: 소수 구하기, Time: 256 ms, Memory: 20672 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -11,24 +11,24 @@ class Main {
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
+        boolean[] notPrimes = new boolean[n + 1];
+        notPrimes[0] = notPrimes[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(notPrimes.length); i++) {
+            if (notPrimes[i]) {
+                continue;
+            }
+            for (int j = i * i; j < notPrimes.length; j += i) {
+                notPrimes[j] = true;
+            }
+        }
+
         StringBuilder sb = new StringBuilder();
         for (int i = m; i <= n; i++) {
-            if (isPrime(i)) {
+            if (!notPrimes[i]) {
                 sb.append(i).append('\n');
             }
         }
         System.out.println(sb);
-    }
-
-    private static boolean isPrime(int num) {
-        if (num < 2) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
