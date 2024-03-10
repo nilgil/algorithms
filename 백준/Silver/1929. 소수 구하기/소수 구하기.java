0508947eb4798900d13ca2a1e4f1f0f99fd67f53
Younggil An <higiri@naver.com>
2024-03-11 01:51:06 +0900

    [Silver III] Title: 소수 구하기, Time: 492 ms, Memory: 18724 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                sb.append(i).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
