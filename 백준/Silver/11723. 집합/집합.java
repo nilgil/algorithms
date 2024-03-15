b22b309d99c1a492c183c227b315102a561ff56f
Younggil An <higiri@naver.com>
2024-03-15 20:27:50 +0900

    [Silver V] Title: 집합, Time: 1088 ms, Memory: 312700 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -4,50 +4,39 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static boolean[] s = new boolean[21];
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        int a = 0; // 20 bit 공간이 필요하기에 32 bit인 int형 사용
+        int a = 0;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String operation = st.nextToken();
-            int operand;
-            switch (operation) {
-                case "add":
-                    operand = Integer.parseInt(st.nextToken());
+            if (st.hasMoreTokens()) {
+                int operand = Integer.parseInt(st.nextToken());
+                if (operation.charAt(0) == 'a') {
                     a = a | (1 << operand);
-                    break;
-                case "remove":
-                    operand = Integer.parseInt(st.nextToken());
+                } else if (operation.charAt(0) == 'r') {
                     a = a & ~(1 << operand);
-                    break;
-                case "toggle":
-                    operand = Integer.parseInt(st.nextToken());
+                } else if (operation.charAt(0) == 't') {
                     if (0 < (1 << operand & a)) {
                         a = a & ~(1 << operand);
                     } else {
                         a = a | (1 << operand);
                     }
-                    break;
-                case "check":
-                    operand = Integer.parseInt(st.nextToken());
+                } else if (operation.charAt(0) == 'c') {
                     if (0 < (1 << operand & a)) {
                         sb.append(1).append('\n');
                     } else {
                         sb.append(0).append('\n');
                     }
-                    break;
-                case "all":
+                }
+            } else {
+                if (operation.charAt(0) == 'a') {
                     a = a | (1 << 21) - 1;
-                    break;
-                case "empty":
+                } else if (operation.charAt(0) == 'e') {
                     a = 0;
-                    break;
+                }
             }
         }
         System.out.println(sb);
