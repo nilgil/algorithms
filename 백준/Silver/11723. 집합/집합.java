87533c456300e35f81a8a1683ea4b41109a421cc
Younggil An <higiri@naver.com>
2024-03-15 20:17:05 +0900

    [Silver V] Title: 집합, Time: 1128 ms, Memory: 312804 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,61 +11,45 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        int a = 0; // 20 bit 공간이 필요하기에 32 bit인 int형 사용
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String operation = st.nextToken();
-            if (!st.hasMoreTokens()) {
-                switch (operation) {
-                    case "all":
-                        all();
-                        break;
-                    case "empty":
-                        empty();
-                        break;
-                }
-                continue;
-            }
-            int operand = Integer.parseInt(st.nextToken());
+            int operand;
             switch (operation) {
                 case "add":
-                    add(operand);
+                    operand = Integer.parseInt(st.nextToken());
+                    a = a | (1 << operand);
                     break;
                 case "remove":
-                    remove(operand);
-                    break;
-                case "check":
-                    sb.append(check(operand)).append('\n');
+                    operand = Integer.parseInt(st.nextToken());
+                    a = a & ~(1 << operand);
                     break;
                 case "toggle":
-                    toggle(operand);
+                    operand = Integer.parseInt(st.nextToken());
+                    if (0 < (1 << operand & a)) {
+                        a = a & ~(1 << operand);
+                    } else {
+                        a = a | (1 << operand);
+                    }
+                    break;
+                case "check":
+                    operand = Integer.parseInt(st.nextToken());
+                    if (0 < (1 << operand & a)) {
+                        sb.append(1).append('\n');
+                    } else {
+                        sb.append(0).append('\n');
+                    }
+                    break;
+                case "all":
+                    a = a | (1 << 21) - 1;
+                    break;
+                case "empty":
+                    a = 0;
                     break;
             }
         }
         System.out.println(sb);
-    }
-
-    private static void add(int operand) {
-        s[operand] = true;
-    }
-
-    private static void remove(int operand) {
-        s[operand] = false;
-    }
-
-    private static int check(int operand) {
-        return s[operand] ? 1 : 0;
-    }
-
-    private static void toggle(int operand) {
-        s[operand] = !s[operand];
-    }
-
-    private static void all() {
-        Arrays.fill(s, true);
-    }
-
-    private static void empty() {
-        Arrays.fill(s, false);
     }
 }
