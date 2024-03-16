692d40b088c65d3f0bf55237821c32c13ebe330a
Younggil An <higiri@naver.com>
2024-03-16 17:59:58 +0900

    [Silver IV] Title: 듣보잡, Time: 288 ms, Memory: 26124 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -15,27 +12,26 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        Map<String, Boolean> map = new HashMap<>();
-
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
-            map.put(br.readLine(), true);
+            set.add(br.readLine());
         }
 
-        int head = 0;
-        String[] anonymous = new String[Math.max(n, m)];
+        List<String> results = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             String name = br.readLine();
-            if (map.get(name) != null) {
-                anonymous[head++] = name;
+            if (set.contains(name)) {
+                results.add(name);
             }
         }
-        Arrays.sort(anonymous, 0, head);
 
-        for (int i = 0; i < head; i++) {
-            sb.append(anonymous[i]).append('\n');
+        Collections.sort(results);
+
+        for (String result : results) {
+            sb.append(result).append('\n');
         }
 
-        System.out.println(head);
+        System.out.println(results.size());
         System.out.println(sb);
     }
 }
