b9927f55f76d98ae39d21016688224faeeaeffb8
Younggil An <higiri@naver.com>
2024-04-14 18:46:26 +0900

    [Silver V] Title: 회문인 수, Time: 132 ms, Memory: 14896 KB -BaekjoonHub

--- 백준/Silver/11068. 회문인 수/회문인 수.java
+++ 백준/Silver/11068. 회문인 수/회문인 수.java
@@ -10,37 +10,37 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
-            boolean flag = false;
+            boolean isPalindrome = false;
             int n = Integer.parseInt(br.readLine());
             for (int i = 2; i <= 64; i++) {
-                if (test(n, i)) {
-                    sb.append(1).append('\n');
-                    flag = true;
+                int[] digits = new int[20];
+                int length = convertBase(n, i, digits);
+                if (isPalindrome(digits, length)) {
+                    isPalindrome = true;
                     break;
                 }
             }
-            if (!flag) {
+            if (isPalindrome) {
+                sb.append(1).append('\n');
+            } else {
                 sb.append(0).append('\n');
             }
         }
         System.out.println(sb);
     }
 
-    public static boolean test(int n, int b) {
-        int head = 0;
-        int[] nums = new int[(int) (Math.sqrt(n) + 1)];
+    public static int convertBase(int n, int base, int[] digits) {
+        int length = 0;
         while (0 < n) {
-            int remainder = n % b;
-            if (remainder < 10) {
-                nums[head++] = remainder + '0';
-            } else {
-                nums[head++] = remainder - 10 + 'A';
-            }
-            n /= b;
+            digits[length++] = n % base;
+            n /= base;
         }
+        return length;
+    }
 
-        for (int i = 0; i < head / 2; i++) {
-            if (nums[i] != nums[head - i - 1]) {
+    private static boolean isPalindrome(int[] digits, int length) {
+        for (int i = 0; i < length / 2; i++) {
+            if (digits[i] != digits[length - i - 1]) {
                 return false;
             }
         }
