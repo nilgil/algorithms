67bfdbc6bda41c76e738a893efa8adb8785688f2
Younggil An <higiri@naver.com>
2024-04-15 23:50:53 +0900

    [Bronze III] Title: ACM 호텔, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+
+            int floor = n % h == 0 ? h : n % h;
+            int ho = n % h == 0 ? n / h : n / h + 1;
+            
+            sb.append(floor * 100 + ho).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
