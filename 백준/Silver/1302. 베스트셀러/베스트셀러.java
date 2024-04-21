cdf389485d4b8a6d0f50428f35ba556342bb5d00
Younggil An <higiri@naver.com>
2024-04-21 17:13:23 +0900

    [Silver IV] Title: 베스트셀러, Time: 120 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Main {
 
@@ -11,21 +12,21 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         Map<String, Integer> sell = new HashMap<>();
-        String[] sort = new String[1001];
         for (int i = 0; i < n; i++) {
             String book = br.readLine();
-            int count = sell.getOrDefault(book, 0) + 1;
-            sell.put(book, count);
-            if (sort[count] == null || 0 < sort[count].compareTo(book)) {
-                sort[count] = book;
-            }
+            sell.put(book, sell.getOrDefault(book, 0) + 1);
         }
 
-        for (int i = 1000; i >= 0; i--) {
-            if (sort[i] != null) {
-                System.out.println(sort[i]);
-                break;
+        String maxTitle = "";
+        int maxCount = 0;
+        for (Entry<String, Integer> entry : sell.entrySet()) {
+            String name = entry.getKey();
+            int count = entry.getValue();
+            if (maxCount < count || maxCount == count && name.compareTo(maxTitle) < 0) {
+                maxTitle = name;
+                maxCount = count;
             }
         }
+        System.out.println(maxTitle);
     }
 }
