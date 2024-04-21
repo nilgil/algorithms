b2b1c8c7253ecea8af23d50ede94e59a98982612
Younggil An <higiri@naver.com>
2024-04-21 16:33:45 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 588 ms, Memory: 45468 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,19 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        Set<String> company = new HashSet<>();
-        StringTokenizer st;
+        Set<String> company = new TreeSet<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
@@ -25,10 +24,12 @@ public class Main {
             }
         }
 
+        String[] ordered = company.toArray(new String[0]);
+
         StringBuilder sb = new StringBuilder();
-        company.stream()
-            .sorted(Comparator.reverseOrder())
-            .forEach(name -> sb.append(name).append('\n'));
+        for (int i = ordered.length - 1; i >= 0; i--) {
+            sb.append(ordered[i]).append('\n');
+        }
 
         System.out.println(sb);
     }
