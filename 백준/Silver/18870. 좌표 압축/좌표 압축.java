837ce43e73178d6c04c2ae862d449d60119aa71b
Younggil An <higiri@naver.com>
2024-04-23 00:27:11 +0900

    [Silver II] Title: 좌표 압축, Time: 2068 ms, Memory: 277808 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,32 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int[] nums = new int[n];
+        Set<Integer> set = new TreeSet<>();
         for (int i = 0; i < n; i++) {
-            int x = Integer.parseInt(st.nextToken());
-            nums[i] = x;
+            nums[i] = Integer.parseInt(st.nextToken());
+            set.add(nums[i]);
         }
 
-        int[] sorted = Arrays.stream(nums)
-            .distinct()
-            .sorted()
-            .toArray();
-
         Map<Integer, Integer> ranks = new HashMap<>();
-        for (int i = 0; i < sorted.length; i++) {
-            ranks.put(sorted[i], i);
+        int index = 0;
+        for (int x : set) {
+            ranks.put(x, index++);
         }
 
         StringBuilder sb = new StringBuilder();
