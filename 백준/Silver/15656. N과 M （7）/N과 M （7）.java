a483eaba12f247f149ee636a8af7762f4da0acf0
Younggil An <nilgil183@gmail.com>
2024-09-26 06:03:25 +0900

    [Silver III] Title: N과 M (7), Time: 560 ms, Memory: 123296 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] nums;
+    static boolean[] check;
+    static int[] output;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        nums = new int[n];
+        check = new boolean[n];
+        output = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        perm(0);
+        System.out.println(sb);
+    }
+
+    static StringBuilder sb = new StringBuilder();
+
+    static void perm(int depth) {
+        // base case
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(output[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        // recursive case
+        for (int i = 0; i < n; i++) {
+            output[depth] = nums[i];
+            perm(depth + 1);
+        }
+    }
+}
