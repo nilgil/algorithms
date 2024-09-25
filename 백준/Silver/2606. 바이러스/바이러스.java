6d0108ad1805765a48c9f177d2701410e78d1ba1
Younggil An <nilgil183@gmail.com>
2024-09-25 14:08:38 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * [DFS/BFS] BJ-2606 바이러스
+ * - 노드의 수는 100 이하인 양의 정수이다.
+ * - 간선의 방향성과 가중치는 없다.
+ * - 1번 노드와 연결된 모든 노드의 수를 구해라.
+ */
+public class Main {
+
+    static int n, m, count = 0;
+    static boolean[][] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        graph = new boolean[n + 1][n + 1];
+        visited = new boolean[n + 1];
+
+        StringTokenizer st;
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            graph[from][to] = true;
+            graph[to][from] = true;
+        }
+
+        recursiveDfs(1);
+
+        System.out.println(count);
+    }
+
+    static void recursiveDfs(int node) {
+        visited[node] = true;
+        for (int i = 1; i <= n; i++) {
+            if (graph[node][i] && !visited[i]) {
+                count++;
+                recursiveDfs(i);
+            }
+        }
+    }
+}
