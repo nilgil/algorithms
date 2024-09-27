bdd90b22b7604c4ac9ee343da0dcd038926e43d0
Younggil An <nilgil183@gmail.com>
2024-09-28 02:25:57 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
@@ -1,44 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int a, k;
-    static int[] counts = new int[1_000_001];
-
-    public static void main(String[] args) throws Exception {
+    private void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        a = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        System.out.println(bfs());
+        int a = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+        while (a!=k) {
+            cnt++;
+            if (k%2==0 && k/2>=a)
+                k/=2;
+            else
+                k--;
+        }
+        System.out.println(cnt);
     }
 
-    static int bfs() {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(a);
-
-        while (!queue.isEmpty()) {
-            Integer now = queue.poll();
-            if (now == k) {
-                return counts[k];
-            }
-
-            int multi = now * 2;
-            if (multi <= k && counts[multi] == 0) {
-                counts[multi] = counts[now] + 1;
-                queue.offer(multi);
-            }
-
-            int add = now + 1;
-            if (add <= k && counts[add] == 0) {
-                counts[add] = counts[now] + 1;
-                queue.offer(add);
-            }
-        }
-        return -1;
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
     }
 }
