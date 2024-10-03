229536eaa60824ee0318e1978dd3f497aa7feb93
Younggil An <nilgil183@gmail.com>
2024-10-03 22:43:32 +0900

    [Silver III] Title: N과 M (4), Time: 124 ms, Memory: 18248 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        selected = new int[m + 1];
+    }
+
+    static int n, m;
+    static int[] selected;
+
+    private static void recursive(int depth) {
+        if (depth == m + 1) {
+            for (int i = 1; i <= m; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            int start = selected[depth - 1];
+            if (start == 0) {
+                start = 1;
+            }
+            for (int i = start; i <= n; i++) {
+                selected[depth] = i;
+                recursive(depth + 1);
+                selected[depth] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+
+        recursive(1);
+
+        System.out.println(sb);
+    }
+}
