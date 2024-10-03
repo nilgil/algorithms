f42a14a4e053e29e8d4b74bc9967add8feae5f6e
Younggil An <nilgil183@gmail.com>
2024-10-03 18:57:24 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 112 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,24 +8,23 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-
-        StringBuilder sb = new StringBuilder();
-        sb.append('<');
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 1; i <= n; i++) {
-            queue.offer(i);
+        int[] nums = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i + 1;
         }
-        while (!queue.isEmpty()) {
-            for (int i = 0; i < k - 1; i++) {
-                queue.offer(queue.poll());
-            }
-            sb.append(queue.poll()).append(',').append(' ');
+
+        int current = k - 1;
+        sb.append('<').append(nums[current]);
+        System.arraycopy(nums, current + 1, nums, current, --n - current);
+        while (n > 0) {
+            current = (current + k - 1) % n;
+            sb.append(',').append(' ').append(nums[current]);
+            System.arraycopy(nums, current + 1, nums, current, --n - current);
         }
-        sb.replace(sb.length() - 2, sb.length(), "");
         sb.append('>');
         System.out.println(sb);
     }
