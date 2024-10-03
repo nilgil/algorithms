61ac6feb364e5b19d5cf7b300c6e01eb91231424
Younggil An <nilgil183@gmail.com>
2024-10-03 23:24:56 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 168 ms, Memory: 18356 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        nums = new int[n];
+        calculated = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        calculated[0] = nums[0];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < operations.length; i++) {
+            operations[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static int n;
+    static int[] nums;
+    static int[] calculated;
+    static int[] operations = new int[4];
+    static int[] used = new int[4];
+    static List<Integer> results = new ArrayList<>();
+
+    private static void recursive(int depth) {
+        if (depth == n) {
+            results.add(calculated[n - 1]);
+        } else {
+            for (int i = 0; i < 4; i++) {
+                if (operations[i] <= used[i]) {
+                    continue;
+                }
+                if (i == 0) {
+                    calculated[depth] = calculated[depth - 1] + nums[depth];
+                } else if (i == 1) {
+                    calculated[depth] = calculated[depth - 1] - nums[depth];
+                } else if (i == 2) {
+                    calculated[depth] = calculated[depth - 1] * nums[depth];
+                } else {
+                    calculated[depth] = calculated[depth - 1] / nums[depth];
+                }
+                used[i]++;
+
+                recursive(depth + 1);
+
+                calculated[depth] = 0;
+                used[i]--;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+
+        recursive(1);
+
+        results.sort(Integer::compareTo);
+        System.out.println(results.get(results.size() - 1));
+        System.out.println(results.get(0));
+    }
+}
