18313bbe568a33a00503ffcc1d05c182b9ba565b
Younggil An <nilgil183@gmail.com>
2024-10-04 01:54:36 +0900

    [Gold IV] Title: N-Queen, Time: 5292 ms, Memory: 14924 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -15,25 +15,33 @@ public class Main {
     static int n, count = 0;
     static int[] col;
 
+    public static void main(String[] args) throws Exception {
+        input();
+
+        recursive(1);
+
+        System.out.println(count);
+    }
+
     private static void recursive(int row) {
         if (row == n + 1) {
             count++;
         } else {
+            // 한줄 단위 컬럼 순회 놓기
             for (int i = 1; i <= n; i++) {
-                // 체크를 로우의 컬럼마다 진행하여 못놓는 곳에 놓는 경우 건너뜀
-                if (check(row, i)) {
-                    col[row] = i;
-                    recursive(row + 1);
-                    col[row] = 0;
+                // 다음줄 놓기전에 해당 좌표에 놔도 되는지 체크하고 놓기
+                if (!available(row, i)) {
+                    continue;
                 }
+                col[row] = i;
+                recursive(row + 1);
+                col[row] = 0;
             }
         }
     }
 
-    private static boolean check(int r, int c) {
-        // 해당 행 이전의 행들을 보고 현재 칸에 놓을 수 있는지 체크
+    private static boolean available(int r, int c) {
         for (int i = 1; i < r; i++) {
-            // col[i]에 각 로우에 어떤 행에 담긴지 기록되어있음
             if (attackable(r, c, i, col[i])) {
                 return false;
             }
@@ -42,28 +50,6 @@ public class Main {
     }
 
     private static boolean attackable(int r1, int c1, int r2, int c2) {
-        // r2가 r1보다 작은게 보장됨
-        // 같은 열에 있다면 공격 가능
-        if (c1 == c2) {
-            return true;
-        }
-
-        // 대각선 검사
-        if (r1 + c1 == r2 + c2) {
-            return true;
-        }
-        if (r1 - c1 == r2 - c2) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static void main(String[] args) throws Exception {
-        input();
-
-        recursive(1);
-
-        System.out.println(count);
+        return c1 == c2 || r1 - c1 == r2 - c2 || r1 + c1 == r2 + c2;
     }
 }
