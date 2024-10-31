6614b6b2e3c99be63dbc70cb09db819fa68261a5
Younggil An <nilgil183@gmail.com>
2024-11-01 01:16:57 +0900

    [Silver II] Title: 예산, Time: 144 ms, Memory: 15180 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -5,11 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	// 3 ~ 10_000
-	static int n;
-	// n ~ 1_000_000_000
-	static int m;
-	// 1 ~ 100_000
+	static int n, m;
 	static int[] requests;
 	static int max = Integer.MIN_VALUE;
 
@@ -18,13 +14,11 @@ public class Main {
 
 		int l = 0;
 		int r = max;
-		int result = 0;
 		while (l <= r) {
 			int c = (l + r) / 2;
 			int sum = 0;
 			for (int i = 0; i < n; i++) {
-				int request = requests[i];
-				sum += Math.min(request, c);
+				sum += Math.min(requests[i], c);
 			}
 			if (sum <= m) {
 				l = c + 1;
