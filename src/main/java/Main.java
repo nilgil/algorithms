ab2644602c758174c6aecdbcaf17d08dfa55faad
nilgil <nilgil183@gmail.com>
2024-11-03 22:07:10 +0900

    init project

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	static int n;
+
+	public static void main(String[] args) throws Exception {
+		input();
+
+	}
+
+	private static void input() throws IOException {
+
+	}
+}
