480c79f94b95bdc6192ddea0ab361ac855213a26
Younggil An <nilgil183@gmail.com>
2024-11-08 01:33:34 +0900

    [Gold IV] Title: Yes or yes, Time: 420 ms, Memory: 65680 KB -BaekjoonHub

--- 백준/Gold/25195. Yes or yes/Yes or yes.java
+++ 백준/Gold/25195. Yes or yes/Yes or yes.java
@@ -5,57 +5,53 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+/*
+ * */
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
 
-	static int n, m, s;
 	static List<List<Integer>> graph = new ArrayList<>();
 	static boolean[] fans;
-	static boolean[] visited;
-	static boolean mustMeet = true;
-
-	public static void main(String[] args) throws Exception {
-		input();
-
-		dfs(1);
-
-		System.out.println(mustMeet ? "Yes" : "yes");
-	}
+	static boolean canAvoid;
 
 	private static void dfs(int node) {
-		if (!mustMeet || fans[node]) {
+		if (canAvoid || fans[node]) {
 			return;
 		}
-		List<Integer> nearNodes = graph.get(node);
-		if (nearNodes.isEmpty()) {
-			mustMeet = false;
+		List<Integer> adjNodes = graph.get(node);
+		if (adjNodes.isEmpty()) {
+			canAvoid = true;
+			return;
 		}
-		for (Integer next : nearNodes) {
-			if (!visited[next]) {
-				visited[node] = true;
-				dfs(next);
-				visited[node] = false;
-			}
+		for (Integer nextNode : adjNodes) {
+			dfs(nextNode);
 		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		input();
+		dfs(1);
+		System.out.println(canAvoid ? "yes" : "Yes");
 	}
 
 	private static void input() throws IOException {
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		for (int i = 0; i <= n; i++) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < n + 1; i++) {
 			graph.add(new ArrayList<>());
 		}
 		fans = new boolean[n + 1];
-		visited = new boolean[n + 1];
-		m = Integer.parseInt(st.nextToken());
+
+		int m = Integer.parseInt(st.nextToken());
 		while (m-- > 0) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			graph.get(u).add(v);
 		}
-		s = Integer.parseInt(br.readLine());
+
+		int s = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		while (s-- > 0) {
 			fans[Integer.parseInt(st.nextToken())] = true;
