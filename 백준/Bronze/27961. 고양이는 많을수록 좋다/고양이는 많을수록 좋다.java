0a47c3538c1b089e07bd72519bd8bb163f24c717
Younggil An <nilgil183@gmail.com>
2024-11-09 23:51:29 +0900

    [Bronze I] Title: 고양이는 많을수록 좋다, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/27961. 고양이는 많을수록 좋다/고양이는 많을수록 좋다.java
+++ 백준/Bronze/27961. 고양이는 많을수록 좋다/고양이는 많을수록 좋다.java
@@ -4,29 +4,20 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	static long n;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Long.parseLong(br.readLine());
+		long n = Long.parseLong(br.readLine());
 		if (n <= 1) {
 			System.out.println(n);
 			return;
 		}
-		dfs(1, 1);
-		System.out.println(min);
-	}
 
-	static int min = Integer.MAX_VALUE;
-
-	private static void dfs(long cats, int count) {
-		if (n < cats) {
-			return;
+		int count = 1;
+		long tmp = 1;
+		while (tmp < n) {
+			tmp *= 2;
+			count++;
 		}
-		if (n <= cats + cats) {
-			min = Math.min(min, count + 1);
-			return;
-		}
-		dfs(cats * 2, count + 1);
+		System.out.println(count);
 	}
 }
