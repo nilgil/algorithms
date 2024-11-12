96f26537e99a1f3400cddb227b6ddb1e2284e537
Younggil An <nilgil183@gmail.com>
2024-11-12 23:50:53 +0900

    [Gold IV] Title: 비슷한 단어, Time: 276 ms, Memory: 22288 KB -BaekjoonHub

---
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Map<String, Integer> words = new HashMap<>();
+		int n = Integer.parseInt(br.readLine());
+		String[] arr = new String[n];
+		for (int i = 0; i < n; i++) {
+			String word = br.readLine();
+			arr[i] = word;
+			words.put(word, words.getOrDefault(word, i));
+		}
+		List<String> sorted = words.keySet().stream()
+			.sorted(String::compareTo)
+			.collect(Collectors.toList());
+
+		int s = -1;
+		int t = -1;
+
+		int maxCount = Integer.MIN_VALUE;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				int count = samePrefixCount(sorted.get(i), sorted.get(j));
+				if (count < maxCount) {
+					break;
+				}
+				if (maxCount < count) {
+					maxCount = count;
+					String s1 = sorted.get(i);
+					String s2 = sorted.get(j);
+					Integer idx1 = words.get(s1);
+					Integer idx2 = words.get(s2);
+					s = Math.min(idx1, idx2);
+					t = Math.max(idx1, idx2);
+				} else {
+					String s1 = sorted.get(i);
+					String s2 = sorted.get(j);
+					Integer idx1 = words.get(s1);
+					Integer idx2 = words.get(s2);
+					int min = Math.min(idx1, idx2);
+					int max = Math.max(idx1, idx2);
+					if (min < s) {
+						s = min;
+						t = max;
+					} else if (min == s && max < t) {
+						t = max;
+					}
+				}
+			}
+		}
+		System.out.println(arr[s]);
+		System.out.println(arr[t]);
+	}
+
+	private static int samePrefixCount(String s1, String s2) {
+		int loopCount = Math.min(s1.length(), s2.length());
+		int sameCount = 0;
+		for (int i = 0; i < loopCount; i++) {
+			char c1 = s1.charAt(i);
+			char c2 = s2.charAt(i);
+			if (c1 == c2) {
+				sameCount++;
+			} else {
+				break;
+			}
+		}
+		return sameCount;
+	}
+}
