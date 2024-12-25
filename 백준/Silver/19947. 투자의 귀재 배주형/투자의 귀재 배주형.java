11455c87f951c5fb7994e889d57694d0a7959672
Younggil An <nilgil183@gmail.com>
2024-12-26 00:05:49 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.java
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.java
@@ -7,20 +7,19 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int pay = Integer.parseInt(st.nextToken());
-		int year = Integer.parseInt(st.nextToken());
-
-		long[] dp = new long[year+1];
-
-		dp[0] = pay;
-
-		for(int i=1; i<=year; i++){
-			dp[i] = (long)Math.floor(dp[i-1] * 1.05);
-			if(i >= 3)
-				dp[i] = Math.max((long)Math.floor(dp[i-3] * 1.2), dp[i]);
-			if(i >= 5)
-				dp[i] = Math.max((long)Math.floor(dp[i-5] * 1.35), dp[i]);
+		int h = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int[] dp = new int[y + 1];
+		dp[0] = h;
+		for (int i = 1; i <= y; i++) {
+			dp[i] = (int)(dp[i - 1] * 1.05);
+			if (i >= 3) {
+				dp[i] = Math.max(dp[i], (int)(dp[i - 3] * 1.2));
+			}
+			if (i >= 5) {
+				dp[i] = Math.max(dp[i], (int)(dp[i - 5] * 1.35));
+			}
 		}
-		System.out.println(dp[year]);
+		System.out.println(dp[y]);
 	}
 }
